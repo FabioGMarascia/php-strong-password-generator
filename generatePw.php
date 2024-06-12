66001b925d4072c84077c54df7d966c7815613f2
FabioGMarascia <fabiogaspare.marascia@gmail.com>
2024-06-12 15:56:00 +0200

    M-1 PW X generator

---
+++ generatePw.php
@@ -0,0 +1,56 @@
+<?php
+
+if (isset($_GET["passwordLength"])) {
+
+    $passwordLength = $_GET["passwordLength"];
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="./css/style.css">
+    <title>PHP STRONG PW-GEN</title>
+</head>
+
+<body>
+
+    <div class="row justify-content-center mt-5">
+        <div class="col-5 px-0 text-center">
+
+            <h1 class="text-warning display-3 fw-bold text-center">GENERATED PASSWORD</h1>
+
+            <div class="row justify-content-center">
+                <div class="col-12">
+
+                    <?php
+                    // METHOD N.1
+                    if (isset($_GET["passwordLength"])) {
+                        $passwordBox = "";
+                        for ($i = 0; $i < $passwordLength; $i++) {
+                            $passwordBox .= "X";
+                        }
+                        echo $passwordBox;
+
+                        // METHOD N.2
+                        // echo str_repeat("X", $passwordLength);
+                    }
+
+                    ?>
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
